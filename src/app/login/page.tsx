e07a4f93dbf1ce975018f984e202b1139dcd383d
Edmand <edmandtantiahhao@gmail.com>
2026-09-18 17:40:18 +0800

    layout fix

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -7,7 +7,7 @@ import Link from "next/link";
 
 const authClient = createAuthClient({
   baseURL:
-    process.env.NEXT_PUBLIC_BETTER_AUTH_URL ||
+    process.env.BETTER_AUTH_URL ||
     (typeof window !== "undefined" ? window.location.origin : ""),
 });
 
